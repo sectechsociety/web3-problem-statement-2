80f1ff1a9d2091c5f7b37e2edf6b311886f932b6
srikathickeyanganapathy <srikarthcikeyanganapathy@gmail.com>
2024-11-09 03:24:36 +0530

    Initial commit

---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const privatekey = process.env.PRIVATE_KEY;
+const infuraKey = process.env.INFURA_API_KEY;
+
+module.exports = {
+    networks: {
+        sepolia: {
+            provider: () => new HDWalletProvider(privatekey, `https://sepolia.infura.io/v3/${infuraKey}`),
+            network_id: 11155111,
+            gas: 5500000,
+            confirmations: 2,
+            networkCheckTimeout: 10000,
+            timeoutBlocks: 200,
+            skipDryRun: true,
+        },
+    },
+    compilers: {
+        solc: {
+            version: "0.8.20",
+        },
+    },
+};
